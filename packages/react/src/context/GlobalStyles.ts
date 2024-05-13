162cdf02d9e53c4dec56da8cfdf0127cd5cca1c5
JessicaMira <144673379+JessicaMira@users.noreply.github.com>
2024-05-13 16:57:55 +0100

    Global styles created

---
+++ packages/react/src/context/GlobalStyles.ts
@@ -0,0 +1,23 @@
+export const globalStyles = {
+  body: {
+    fontFamily: 'Arial, sans-serif',
+    margin: 0,
+    padding: 0,
+    color: '#484848',
+  },
+  '*': {
+    boxSizing: 'border-box',
+  },
+  h1: {
+    fontSize: '2rem',
+    fontWeight: 'bold',
+  },
+  h2: {
+    fontSize: '1.5rem',
+    fontWeight: 'bold',
+  },
+  h3: {
+    fontSize: '1rem',
+    fontWeight: 'bold',
+  },
+};
