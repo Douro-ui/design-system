639518b7dc1a5595590f3c82926280fac98f0c2c
Arthurmqz <Arthurmqz@users.noreply.github.com>
2024-06-21 10:22:09 +0000

    feat: checkbox component

---
+++ packages/components/checkbox/src/Checkbox.tsx
@@ -0,0 +1,49 @@
+import type { CheckboxProps } from './checkbox.types';
+import { InputStyled, LabelStyled, CheckboxContainerStyled } from './checkbox.styles';
+import { useCallback } from 'react';
+
+const Checkbox = ({
+  backgroundColor,
+  label,
+  onChange,
+  checked,
+  name,
+  value,
+  disabled,
+  isCircle,
+  ...props
+}: CheckboxProps) => {
+  const handleChange = useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      if (!disabled) {
+        onChange?.(event);
+      }
+    },
+    [disabled, onChange]
+  );
+
+  return (
+    <CheckboxContainerStyled
+      backgroundColor={backgroundColor}
+    >
+      <label>
+        <InputStyled
+          {...props}
+          type="checkbox"
+          id={value}
+          name={name}
+          value={value}
+          checked={checked}
+          onChange={handleChange}
+          disabled={disabled}
+          isCircle={isCircle}
+        />
+        <LabelStyled>
+          {label}
+        </LabelStyled>
+      </label>
+    </CheckboxContainerStyled>
+  );
+};
+
+export default Checkbox;
---
+++ packages/components/checkbox/src/CheckboxGroup.stories.tsx
@@ -0,0 +1,69 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import CheckboxGroup from './CheckboxGroup';
+import { useState } from 'react';
+
+const meta: Meta<typeof CheckboxGroup> = {
+  title: 'Example/CheckboxGroup',
+  component: CheckboxGroup,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+const CheckboxGroupWithHooks = () => {
+  const [selected, setSelected] = useState<string[]>([]);
+
+  const handleChange = (values: string[]) => {
+    action('checkbox-group-changed')(values);
+    setSelected(values);
+  };
+
+  return (
+    <CheckboxGroup
+      name="checkboxGroup"
+      options={[
+        { label: 'Checkbox 1', value: 'checkbox1' },
+        { label: 'Checkbox 2', value: 'checkbox2' },
+        { label: 'Checkbox 3', value: 'checkbox3', disabled: true },
+      ]}
+      selectedValues={selected}
+      onChange={handleChange}
+    />
+  );
+};
+
+const CircleCheckboxGroupWithHooks = () => {
+  const [selected, setSelected] = useState<string[]>([]);
+
+  const handleChange = (values: string[]) => {
+    action('checkbox-group-changed')(values);
+    setSelected(values);
+  };
+
+  return (
+    <CheckboxGroup
+      name="checkboxGroup"
+      options={[
+        { label: 'Checkbox 1', value: 'checkbox1', isCircle: true },
+        { label: 'Checkbox 2', value: 'checkbox2', isCircle: true },
+        { label: 'Checkbox 3', value: 'checkbox3', isCircle: true, disabled: true },
+      ]}
+      selectedValues={selected}
+      onChange={handleChange}
+    />
+  );
+};
+
+export const DefaultCheckboxGroup: Story = {
+  render: () => <CheckboxGroupWithHooks />,
+};
+
+export const CircleCheckboxGroup: Story = {
+  render: () => <CircleCheckboxGroupWithHooks />,
+};
