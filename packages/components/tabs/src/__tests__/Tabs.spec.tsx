09e75bfc8a78a7426a58d5d5fc4b6b4b505af427
JessicaMira <144673379+JessicaMira@users.noreply.github.com>
2024-06-27 14:48:44 +0100

    feat: tabs component

--- packages/components/tabs/src/__tests__/Tabs.spec.tsx
+++ packages/components/tabs/src/__tests__/Tabs.spec.tsx
@@ -5,13 +5,12 @@ describe('<Tabs />', () => {
   it('renders a tab with correct label and attributes', () => {
     const handleChange = jest.fn();
 
-    render(<Tabs label="Tab 1" value="tab1" onClick={handleChange} selected />);
+    render(<Tabs label="Tab 1" value="tab1" onClick={handleChange} />);
 
     const tabElement = screen.getByText('Tab 1');
 
     expect(tabElement).toBeInTheDocument();
     expect(tabElement).toHaveAttribute('value', 'tab1');
-    expect(tabElement).toHaveAttribute('selected', 'false');
     expect(tabElement).not.toHaveAttribute('disabled');
   });
 
