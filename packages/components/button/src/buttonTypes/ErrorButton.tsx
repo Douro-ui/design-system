c68aaeeeaff3e44bf905ba9335d828bbed11fe10
Junior Dias de Oliveira <junior.oliveira@metyis.com>
2024-07-25 21:30:39 +0100

    feat: button component

---
+++ packages/components/button/src/buttonTypes/ErrorButton.tsx
@@ -0,0 +1,44 @@
+import type { ButtonProps, ButtonStyledProps } from '../button.types';
+import { ButtonStyled } from '../button.styles';
+import { deepMerge, useTheme } from '@douro-ui/react';
+import React from 'react';
+
+export const ErrorButton = ({
+  typeBtn,
+  size,
+  children,
+  styled,
+  onClick,
+  disabled,
+  ...props
+}: ButtonProps): React.ReactNode => {
+  const theme = useTheme();
+
+  const getErrorButtonTypeDefaultThemeValues: ButtonStyledProps = {
+    color: theme.colors.brand.white,
+    backgroundColor: theme.colors.extended.red.shade50,
+    colorHover: theme.colors.brand.white,
+    borderColor: theme.colors.extended.red.shade50,
+    backgroundColorDisabled: theme.colors.neutral.cold.shade95,
+    colorDisabled: theme.colors.neutral.cold.shade70,
+  };
+
+  const mergedThemeValues = deepMerge<ButtonStyledProps>(
+    getErrorButtonTypeDefaultThemeValues,
+    styled,
+  );
+
+  return (
+    <ButtonStyled
+      typeBtn={'error'}
+      size={size}
+      styled={mergedThemeValues}
+      onClick={onClick}
+      disabled={disabled}
+      data-testid={`button-${typeBtn}`}
+      {...props}
+    >
+      {children}
+    </ButtonStyled>
+  );
+};
---
+++ packages/components/button/src/buttonTypes/TertiaryButton.tsx
@@ -0,0 +1,47 @@
+import type { ButtonProps, ButtonStyledProps } from '../button.types';
+import { ButtonStyled } from '../button.styles';
+import { deepMerge, useTheme } from '@douro-ui/react';
+import React from 'react';
+
+export const TertiaryButton = ({
+  typeBtn,
+  size,
+  children,
+  styled,
+  onClick,
+  disabled,
+  ...props
+}: ButtonProps): React.ReactNode => {
+  const theme = useTheme();
+
+  const getTertiaryButtonTypeDefaultThemeValues: ButtonStyledProps = {
+    color: theme.colors.brand.primary,
+    backgroundColor: 'transparent',
+    borderColor: 'transparent',
+    backgroundColorActive: 'transparent',
+    colorHover: theme.colors.brand.black,
+    backgroundColorHover: theme.colors.neutral.cold.shade95,
+    backgroundColorDisabled: theme.colors.neutral.cold.shade95,
+    borderColorActive: 'transparent',
+    colorDisabled: theme.colors.neutral.cold.shade70,
+  };
+
+  const mergedThemeValues = deepMerge<ButtonStyledProps>(
+    getTertiaryButtonTypeDefaultThemeValues,
+    styled,
+  );
+
+  return (
+    <ButtonStyled
+      typeBtn={'tertiary'}
+      size={size}
+      styled={mergedThemeValues}
+      onClick={onClick}
+      disabled={disabled}
+      data-testid={`button-${typeBtn}`}
+      {...props}
+    >
+      {children}
+    </ButtonStyled>
+  );
+};
