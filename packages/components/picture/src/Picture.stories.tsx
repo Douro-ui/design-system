5294fa9e3bc226115de56c2061eeb2e1bc6060c0
Manuela Sousa <manuelacatsousa@gmail.com>
2024-08-22 09:40:53 +0100

    test: picure ui tests

--- packages/components/picture/src/Picture.stories.tsx
+++ packages/components/picture/src/Picture.stories.tsx
@@ -3,6 +3,7 @@ import Picture from './Picture';
 import { ThemeProvider } from '@douro-ui/react';
 import { PartialStoryFn } from 'storybook/internal/types';
 import { PictureProps } from './picture.types';
+import { expect, within } from '@storybook/test';
 
 const meta: Meta<typeof Picture> = {
   title: 'Example/Picture',
@@ -53,4 +54,33 @@ export const ImageDisabled: Story = {
     alt: 'A descriptive alt text',
     disabled: true,
   },
+};
+async function testImage(canvasElement: HTMLElement) {
+  const canvas = within(canvasElement);
+  const picture = canvas.getByRole('img');
+
+  expect(picture).toBeVisible();
+
+  expect(picture).toHaveAttribute(
+    'src',
+    'https://gratisography.com/wp-content/uploads/2024/01/gratisography-cyber-kitty-800x525.jpg',
+  );
+
+  expect(picture).toHaveAttribute('alt', 'A descriptive alt text');
+  expect(picture).toHaveStyle('display: block');
+  expect(picture).toHaveStyle('position: static');
+  expect(picture).toHaveStyle('box-sizing: content-box');
+  expect(picture).toHaveStyle('max-height: none');
+  expect(picture).toHaveStyle('height: 525px');
+  expect(picture).toHaveStyle('width: 800px');
+}
+Image.play = async ({ canvasElement }: { canvasElement: HTMLElement }) => {
+  testImage(canvasElement);
+};
+ImageDisabled.play = async ({
+  canvasElement,
+}: {
+  canvasElement: HTMLElement;
+}) => {
+  testImage(canvasElement);
 };
