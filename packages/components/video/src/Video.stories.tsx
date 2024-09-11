53e4a7fbb7800955576db7ab9bad845b0ef04de0
Manuela Sousa <manuelacatsousa@gmail.com>
2024-09-11 17:50:51 +0100

    test: video ui tests

--- packages/components/video/src/Video.stories.tsx
+++ packages/components/video/src/Video.stories.tsx
@@ -3,6 +3,7 @@ import Video from './Video';
 import { ThemeProvider } from '@douro-ui/react';
 import { PartialStoryFn } from 'storybook/internal/types';
 import { VideoProps } from './video.types';
+import { expect, userEvent, within } from '@storybook/test';
 
 const meta: Meta<typeof Video> = {
   title: 'Example/Video',
@@ -77,4 +78,50 @@ export const VideoDisabled: Story = {
     muted: false,
     disabled: true,
   },
+};
+const playVideo = async ({
+  canvasElement,
+  isEnabled,
+}: {
+  canvasElement: HTMLElement;
+  isEnabled: boolean;
+}) => {
+  const canvas = within(canvasElement);
+  const videoContainer = await canvas.findByTestId('video-container-style');
+
+  expect(videoContainer).toBeInTheDocument();
+  expect(videoContainer).toHaveAttribute(
+    'src',
+    'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4',
+  );
+  expect(videoContainer).toHaveStyle({
+    width: '300px',
+    height: '150px',
+    display: 'block',
+  });
+  expect(videoContainer).toHaveAttribute('poster', 'poster.jpg');
+
+  if (isEnabled) {
+    expect(videoContainer).toBeEnabled();
+    userEvent.hover(videoContainer);
+    userEvent.click(videoContainer);
+  } else {
+    expect(videoContainer).toHaveStyle('cursor: not-allowed');
+  }
+};
+
+VideoEnabled.play = async ({
+  canvasElement,
+}: {
+  canvasElement: HTMLElement;
+}) => {
+  await playVideo({ canvasElement, isEnabled: true });
+};
+
+VideoDisabled.play = async ({
+  canvasElement,
+}: {
+  canvasElement: HTMLElement;
+}) => {
+  await playVideo({ canvasElement, isEnabled: false });
 };
